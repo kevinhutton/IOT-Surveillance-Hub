2815aeee44cc634d1fbc517f69793bae981a00f8
chris rehfeld <rehfeldchris@gmail.com>
2017-04-10 20:47:19 -0700

    add bower libs, and redo take a pic page using bootstrap. Also, prototype using new file storage api.

--- js/main.js
+++ js/main.js
@@ -11,3 +11,26 @@ $(document).ready(function(){
     });
 
 });
+
+
+// Add "active" class to nav link that matches current url.
+$(function () {
+    var pathName = window.location.pathname;
+    $(".nav a").each(function () {
+        if (_.includes(pathName, $(this).attr("href"))) {
+            $(this).closest("li").addClass("active");
+        }
+    });
+});
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        var dfd = $.Deferred();
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+            dfd.resolve(animationName, this);
+        });
+        return dfd;
+    }
+});
