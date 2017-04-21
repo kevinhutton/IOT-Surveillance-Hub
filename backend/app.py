b62bf9de42d501b6dc5054fc876cc9d0a195ab5c
kevinhutton <kevinrajahutton@gmail.com>
2017-04-21 07:01:05 +0000

    Add IP geolocation service to backend REST API

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
+#IOT Surveillance Hub Backend
+
 import os
 import json
+import urllib2
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-from flask import Flask
+from flask import Flask,abort
 from flask.ext.cors import CORS
 cloudinary.config( 
   cloud_name = "kjh707", 
@@ -14,16 +17,26 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'IOT Surveillance Hub Backend API'
 @app.route('/admin/take_picture',methods=['GET','POST'])
 def take_picture():
     filename = "pictures/cam.jpg"
     os.system("raspistill -o %s" % filename)
     jsonResponse = cloudinary.uploader.upload(filename)
     return json.dumps(jsonResponse)
+
+# Determine the approxmate coordinates of this device via IP Gelocation
+@app.route('/admin/geolocation',methods=['GET','POST'])
+def geolocation():
+    try:
+	response = urllib2.urlopen('http://ip-api.com/json')
+	data = json.load(response)
+	return json.dumps(data)
+    except:
+	abort(404)
+
 if __name__ == '__main__':
-    # Secret key to encrypt session variables
     app.debug = True
     app.run(port=80)
 
