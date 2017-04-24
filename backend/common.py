4dc5d679e881dcd358869ba47b1debc82189f453
kevinhutton <kevinrajahutton@gmail.com>
2017-04-24 05:22:50 +0000

    Add sqlite3 support

--- backend/common.py
+++ backend/common.py
@@ -4,6 +4,7 @@ import urllib2
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import sqlite3
 from tempfile import NamedTemporaryFile
 
 cloudinary.config(
@@ -11,6 +12,7 @@ cloudinary.config(
     api_key="294731653526325",
     api_secret="XdEkW_0LTsf52PZPyhG3vn-P1YA"
 )
+dbLocation = '/var/lib/iot-project/iot-project.db'
 
 
 def take_picture(tag=None):
@@ -20,3 +22,31 @@ def take_picture(tag=None):
         tmpFile.name, tag="motion-detected")
     tmpFile.close()
     return json.dumps(jsonResponse)
+
+def createBluetoothDevicesTable():
+    conn = sqlite3.connect(dbLocation)
+    print "Opened database successfully";
+    conn.execute('''CREATE TABLE BluetoothDevices 
+	       (ID INT PRIMARY KEY     NOT NULL,
+	       MAC           TEXT    NOT NULL,
+	       DEVICENAME          TEXT     NOT NULL,
+	       DEVICETYPE       TEXT)''')
+    print "Table created successfully";
+    conn.close()
+
+def createNotificationsTable():
+    conn = sqlite3.connect(dbLocation)
+    print "Opened database successfully";
+    conn.execute('''CREATE TABLE Notifications 
+	       (ID INT PRIMARY KEY     NOT NULL,
+	       email           TEXT    NOT NULL)''')
+    print "Table created successfully";
+    conn.close()
+
+def query(queryString):
+    conn = sqlite3.connect(dbLocation)
+    c = conn.execute(queryString)
+    result = c.fetchall()
+    conn.commit()
+    conn.close()
+    return result
