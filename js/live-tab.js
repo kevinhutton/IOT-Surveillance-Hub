162e136c42096604f71df4ec7de716f7abff9cd8
chris <chrehfel@us.ibm.com>
2017-04-26 17:47:45 -0700

    update param in live

--- js/live-tab.js
+++ js/live-tab.js
@@ -20,7 +20,7 @@ $(function setupLiveTabBehavior() {
             url: piCam.fileStoreHost + "/file-store/search.php",
             data: {
                 limit: 1,
-                streamName: streamName
+                tag: streamName
             }
         }).then(function getLatestFileNameSuccess(files) {
             if (!files || !files.length) {
